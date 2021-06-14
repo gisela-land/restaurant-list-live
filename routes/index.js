e9eac8e4d6c69182899437979dc0a62feeb49225
gisela-land <takeabreathe@hotmail.com>
2021-06-14 09:11:53 +0800

    feat: modify home page layout & add sort function

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home.js')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search.js')
+const sort = require('./modules/sort.js')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/sort', sort)
 
 module.exports = router
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -6,13 +6,17 @@ const Restaurant = require('../../models/restaurant.js')
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
 
-  return Restaurant.find({ name: { $regex: keyword, $options: 'i' } })
-    .lean()
-    .then((restaurants) => {
-      const hasResults = restaurants.length > 0
-      return res.render('index', { restaurants: restaurants, keyword: keyword, hasResults: hasResults })
-    })
-    .catch((error) => console.log(error))
+  if (keyword === "") {
+    return res.redirect('/')
+  } else {
+    return Restaurant.find({ name: { $regex: keyword, $options: 'i' } })
+      .lean()
+      .then((restaurants) => {
+        const hasResults = restaurants.length > 0
+        return res.render('index', { restaurants: restaurants, keyword: keyword, hasResults: hasResults, isSearch: true })
+      })
+      .catch((error) => console.log(error))
+  }
 })
 
 module.exports = router
